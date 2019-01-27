7e4703f41f19ee3000f37405899e231f5542f23b
plescasergiu <sergiuplesca1993@mail.ru>
2019-01-27 14:24:17 +0200

    ver 0.5

---
+++ src/homework/Main.java
@@ -0,0 +1,14 @@
+package homework;
+
+public class Main {
+
+
+    
+    public static void  main(String...args){
+
+        ReadFromFile readFromFile = new ReadFromFile();
+        readFromFile.readFromFile();
+
+
+    }
+}
---
+++ src/homework/Person.java
@@ -0,0 +1,13 @@
+package homework;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String birthDay;
+
+    public Person(String firstName, String lastName, String birthDay) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDay = birthDay;
+    }
+}
