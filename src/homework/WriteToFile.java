b74d12a84a8c7e41469466db9868e759a3467d1f
plescasergiu <sergiuplesca1993@mail.ru>
2019-02-03 17:02:45 +0200

    ver 0.7

--- src/homework/WriteToFile.java
+++ src/homework/WriteToFile.java
@@ -2,51 +2,35 @@ package homework;
 
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.nio.file.*;
 import java.util.stream.Stream;
-
 import static java.nio.file.Files.lines;
 
 
 public class WriteToFile {
 
 
-    public void writeToFile(String birthday) {
+    public void writeToFile(String month) {
 
-        String[] brd = birthday.split("-");
-        String sort = brd[0];
+        String search = month;
 
-        List<String> list = new ArrayList<>();
-
-        //reading text file into stream, try-with-resources
         try (Stream<String> stream = lines(Paths.get("C:\\Java\\HomeWorkStream\\Read.csv"))) {
-            list = stream
-                    .filter(line -> line.contains(sort))
+            stream
+                    .filter(x -> x.contains(search))
                     .sorted()
-                    .collect(Collectors.toList());
-
-            list.forEach(System.out::println);
-            for (String i : list) {
-                String[] lines = i.split(",");
-                String output = lines[0] + "-" + lines[1];
-                try {
-
-                    Files.write(Paths.get("C:\\Java\\HomeWorkStream\\Write.csv"), output.getBytes());
-
-
-                } catch (IllegalAccessError e) {
-                    e.printStackTrace();
-
-                }
-            }
-
+                    .forEach(x -> {
+                        String[] data = x.split(",");
+                        String output = data[0] + " " + data[1] + "\n";
+                        try {
+                            Files.write(Path.of("C:\\Java\\HomeWorkStream\\Write.csv"), output.getBytes(), StandardOpenOption.APPEND);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
+
 
     }
 }
