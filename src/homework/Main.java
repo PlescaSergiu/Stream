766bd14595a2f38f77d8295d49bcbfe5b96d1d98
plescasergiu <sergiuplesca1993@mail.ru>
2019-02-03 17:04:57 +0200

    ver 0.7

--- src/homework/Main.java
+++ src/homework/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void  main(String...args){
 
     WriteToFile writeToFile =  new WriteToFile();
-    writeToFile.writeToFile("19");
+    writeToFile.writeToFile("04");
     }
 }
